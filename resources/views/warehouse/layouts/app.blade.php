fdcb11c52cef993a4da07f37788d9cf838993feb
KNinson <kelvinkojoninson@gmail.com>
2026-05-13 06:25:45 +0000

    Render head scripts in warehouse layout

--- resources/views/warehouse/layouts/app.blade.php
+++ resources/views/warehouse/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    @stack('head-scripts')
     @vite(['resources/css/pages/warehouse-portal.css', 'resources/js/warehouse/app.js'])
     @stack('styles')
 </head>
